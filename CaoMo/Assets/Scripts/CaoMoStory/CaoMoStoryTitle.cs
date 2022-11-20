f2c416609fdd10ef6d781a74f5ba8234185b7247
LeoAtopos <275142760@qq.com>
2022-11-20 20:07:42 +0800

    曹沫开场，简单完成

--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoStoryTitle.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoStoryTitle.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class CaoMoStoryTitle : MonoBehaviour
 {
     public AudioSource openSound;
+    public GameObject tapToGo;
+
+    bool isShowFinished = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tapToGo.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isShowFinished)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                Debug.Log("haha");
+            }
+        }
         
     }
 
     public void PlayOpenSound()
     {
         openSound.Play();
+    }
+
+    public void ShowFinished()
+    {
+        tapToGo.SetActive(true);
+        isShowFinished = true;
     }
 }
