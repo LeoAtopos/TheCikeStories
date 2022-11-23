4e7de94753b23be4214174f677095be05ae6354d
LeoAtopos <275142760@qq.com>
2022-11-24 01:09:24 +0800

    简单的进了鲁

--- CaoMo/Assets/Scripts/CaoMoStory/ChooseLuState.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/ChooseLuState.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ChooseLuState : MonoBehaviour
 {
+    public GameObject chooseInUniverse;
+    public GameObject chooseOnEarth;
+    public GameObject chooseOnStateMap;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        chooseOnEarth.SetActive(false);
+        chooseOnStateMap.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,10 +24,34 @@ public class ChooseLuState : MonoBehaviour
     public void ChoosedEarth()
     {
         Debug.Log("earth");
+        chooseInUniverse.GetComponent<Animator>().Play("FaintAway");
+        chooseOnEarth.SetActive(true);
     }
 
     public void ChoosedMoon()
     {
         Debug.Log("moon");
+    }
+
+    public void ChooseInUniverseFade()
+    {
+        chooseInUniverse.SetActive(false);
+    }
+
+
+    public void ChoosedStates()
+    {
+        chooseOnEarth.GetComponent<Animator>().Play("FaintAway");
+        chooseOnStateMap.SetActive(true);
+    }
+
+    public void ChooseOnEarthFade()
+    {
+        chooseOnEarth.SetActive(false);
+    }
+
+    public void ChoosedLu()
+    {
+        chooseOnStateMap.GetComponent<Animator>().Play("ZoomUp");
     }
 }
