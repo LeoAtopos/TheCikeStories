5d28d0d74b2f5a0db6ab0daea8843f237fa5c967
LeoOnWindows <275142760@qq.com>
2022-12-27 14:30:58 +0800

    正常开始移动了

--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoSpriteController.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoSpriteController.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class CaoMoSpriteController : MonoBehaviour
 {
     public Animator animator;
+    public bool isOKToMove = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOKToMove = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(isOKToMove)
         {
-            if(Input.mousePosition.x - transform.position.x < -5.0f)
+            if(Input.GetMouseButton(0))
             {
-                //left move
-                transform.position += new Vector3(-250 * Time.deltaTime, 0 , 0);
-                animator.Play("CaoMoMoving");
-            }else if(Input.mousePosition.x - transform.position.x > 5.0f)
-            {
-                //right move
-                transform.position += new Vector3( 250 * Time.deltaTime, 0 , 0);
-                animator.Play("CaoMoMoving");
+                if(Input.mousePosition.x - transform.position.x < -5.0f)
+                {
+                    //left move
+                    transform.position += new Vector3(-250 * Time.deltaTime, 0 , 0);
+                    animator.Play("CaoMoMoving");
+                }else if(Input.mousePosition.x - transform.position.x > 5.0f)
+                {
+                    //right move
+                    transform.position += new Vector3( 250 * Time.deltaTime, 0 , 0);
+                    animator.Play("CaoMoMoving");
+                }
             }
         }
+    }
+
+    public void SetOkToMove()
+    {
+        isOKToMove = true;
     }
 }
