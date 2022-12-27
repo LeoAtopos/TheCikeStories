1e6361877fa77a46cebe3010bb27c57429a6823c
LeoOnWindows <275142760@qq.com>
2022-12-27 13:13:27 +0800

    曹沫动一动

---
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoSpriteController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaoMoSpriteController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            if(Input.mousePosition.x - transform.position.x < -5.0f)
+            {
+                //left move
+                transform.position += new Vector3(-250 * Time.deltaTime, 0 , 0);
+            }else if(Input.mousePosition.x - transform.position.x > 5.0f)
+            {
+                //right move
+                transform.position += new Vector3( 250 * Time.deltaTime, 0 , 0);
+            }
+        }
+    }
+}
