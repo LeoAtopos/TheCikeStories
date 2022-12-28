caa165079b42325e846c35065b6ea83f9f80bda0
LeoOnWindows <275142760@qq.com>
2022-12-28 12:03:55 +0800

    鲁庄公的马车加入战斗
    服了，还是得靠dotween

--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoBodyShowUpCtrl.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoBodyShowUpCtrl.cs
@@ -14,7 +14,7 @@ public class CaoMoBodyShowUpCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ShowUpEnd()
--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoSpriteController.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoSpriteController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CaoMoSpriteController : MonoBehaviour
 {
     public Animator animator;
     public bool isOKToMove = false;
+
+    public GameObject maChe;
+    public GameObject zhuangTitleText;
+    public GameObject zhuangLines;
     // Start is called before the first frame update
     void Start()
     {
         isOKToMove = false;
+        zhuangLines.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,11 +37,52 @@ public class CaoMoSpriteController : MonoBehaviour
                     animator.Play("CaoMoMoving");
                 }
             }
+
+            if(transform.position.x < 300.0f)
+            {
+                isOKToMove = false;
+                maChe.transform.SetParent(transform);
+                PlayInToStep2();
+            }
         }
     }
 
     public void SetOkToMove()
     {
         isOKToMove = true;
+    }
+
+    void PlayInToStep2()
+    {
+        transform.DOLocalMove(new Vector3(287f,0.0f,0.0f),4).SetEase(Ease.InOutQuad).OnComplete( () => ShowZhuangTitle());
+    }
+
+    void ShowZhuangTitle()
+    {
+        zhuangTitleText.SetActive(true);
+        Invoke("HideZhuangTitle", 1.0f);
+    }
+    void HideZhuangTitle()
+    {
+        zhuangTitleText.SetActive(false);
+        Invoke("ShowZhuangLine", 1.0f);
+    }
+
+    void ShowZhuangLine()
+    {
+        zhuangLines.SetActive(true);
+        Invoke("HideZhuangLine", 2.0f);
+    }
+
+    void HideZhuangLine()
+    {
+        zhuangLines.SetActive(false);
+        Invoke("ZoomOut",1.0f);
+    }
+
+    void ZoomOut()
+    {
+        transform.DOScale(new Vector3(0.5f,0.5f,0), 1.0f);
+        transform.DOLocalMove(new Vector3(0.0f,0.0f,0), 1.0f);
     }
 }
