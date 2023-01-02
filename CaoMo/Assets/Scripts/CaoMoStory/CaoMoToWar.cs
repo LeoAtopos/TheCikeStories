570b29d31b724ae5101b579dcef0f084fd832bb5
LeoAtopos <275142760@qq.com>
2023-01-02 20:31:52 +0800

    曹沫为鲁将好想好定了，还差点动画bug和后面cut scene的时间过长了

--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoToWar.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoToWar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class CaoMoToWar : MonoBehaviour
 {
@@ -10,16 +11,69 @@ public class CaoMoToWar : MonoBehaviour
     public GameObject caoMoBodyNormal;
     public GameObject caoMoBodyArmed;
     public GameObject maChe;
+
+    public Animator horseAnimator;
+    public bool isOKToMove = false;
+    public GameObject trees;
+    public GameObject maCheBingPos;
+
+    public bool isOKTOCharge = false;
+    public GameObject caoMoLine;
+    public bool isChargeDone = false;
+    public bool isCuttingScene = false;
     // Start is called before the first frame update
     void Start()
     {
+        isOKToMove = false;
+        isOKTOCharge = false;
+        caoMoLine.SetActive(false);
+        isChargeDone = false;
+        isCuttingScene = false;
         Invoke("NormalFitFlyAway", 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isOKToMove)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Input.mousePosition.x > transform.position.x + 10.0f)
+                {
+                    horseAnimator.Play("HorseMoving");
+                    trees.transform.position += new Vector3(-300 * Time.deltaTime, 0, 0);
+                }
+            }
+            if (trees.transform.localPosition.x <= -1400.0f)
+            {
+                isOKToMove = false;
+                maCheBingPos.transform.DOLocalMove(new Vector3(990f, 0, 0), 1f).OnComplete(() => MaCheBingGethered());
+            }
+        }
+        if (isOKTOCharge)
+        {
+
+            horseAnimator.Play("HorseMoving");
+            trees.transform.position += new Vector3(-300 * Time.deltaTime, 0, 0);
+
+            if (trees.transform.localPosition.x <= -3500.0f)
+            {
+                isChargeDone = true;
+                isOKTOCharge = false;
+            }
+        }
+        if(isChargeDone)
+        {
+            isCuttingScene = true;
+            transform.DOLocalMove(new Vector3(-2000, 0, 0), 5).OnComplete(() => CutScene());
+            isChargeDone = false;
+        }
+        if(isCuttingScene)
+        {
+            horseAnimator.Play("HorseMoving");
+            trees.transform.position += new Vector3(-300 * Time.deltaTime, 0, 0);
+        }
     }
     void NormalFitFlyAway()
     {
@@ -36,10 +90,33 @@ public class CaoMoToWar : MonoBehaviour
     void MaCheFlyIn()
     {
         maChe.transform.DOLocalMove(new Vector3(129.9f, -87.6f, 0), 1f);
+        maChe.transform.SetParent(transform);
         Invoke("ZoomOut", 2f);
     }
     void ZoomOut()
     {
-
+        transform.DOScale(0.5f, 0.5f).OnComplete(() => OKToMove());
+    }
+    void OKToMove()
+    {
+        isOKToMove = true;
+    }
+    void MaCheBingGethered()
+    {
+        Invoke("ShowLine", 1f);
+        Invoke("ChargeOn", 2.5f);
+    }
+    void ShowLine()
+    {
+        caoMoLine.SetActive(true);
+    }
+    void ChargeOn()
+    {
+        isOKTOCharge = true;
+        caoMoLine.SetActive(false);
+    }
+    void CutScene()
+    {
+        SceneManager.LoadScene("105FightQi");
     }
 }
