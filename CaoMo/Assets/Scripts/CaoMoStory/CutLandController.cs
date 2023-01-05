59bbab004494da01c00587e10816b5441ec873f6
LeoAtopos <275142760@qq.com>
2023-01-05 23:48:44 +0800

    声音准备好。明天再说了，没有好的交互，就直接做动画了

---
+++ CaoMo/Assets/Scripts/CaoMoStory/CutLandController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutLandController : MonoBehaviour
+{
+    public GameObject subTextBoard;
+    public GameObject subText1;
+    public GameObject subText2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        subText1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
