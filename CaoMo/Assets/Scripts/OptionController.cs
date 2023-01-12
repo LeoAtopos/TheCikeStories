291c025ae4df92146c16eeab2116bd7b56ad4deb
LeoAtopos <275142760@qq.com>
2023-01-12 19:12:06 +0800

    2选项

--- CaoMo/Assets/Scripts/OptionController.cs
+++ CaoMo/Assets/Scripts/OptionController.cs
@@ -8,7 +8,10 @@ public class OptionController : MonoBehaviour, IPointerClickHandler
     public MakeMeetingController mMC;
     public void OnPointerClick(PointerEventData eventData)
     {
-        mMC.HuanAns(1);
+        if(gameObject.name == "Option1")
+            mMC.HuanAns(1);
+        if (gameObject.name == "Option2")
+            mMC.HuanAns(2);
     }
 
     // Start is called before the first frame update
