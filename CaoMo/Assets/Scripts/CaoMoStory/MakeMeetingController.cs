2fe3ef6efab1de712ede2a7dc36107d1822554f2
LeoAtopos <275142760@qq.com>
2023-01-12 12:06:23 +0800

    舞台切入

--- CaoMo/Assets/Scripts/CaoMoStory/MakeMeetingController.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/MakeMeetingController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MakeMeetingController : MonoBehaviour
 {
+    public GameObject stage;
+    public GameObject huanPos;
+    public GameObject huanTitleText;
+    public GameObject guanPos;
+    public GameObject guanTitleText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stage.transform.DOLocalMove(new Vector3(0, 0, 0), 8f).OnComplete(()=> StageInDone());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void StageInDone()
+    {
+        Invoke("HideTitles", 5);
+    }
+    void HideTitles()
+    {
+        huanTitleText.SetActive(false);
+        guanTitleText.SetActive(false);
     }
 }
