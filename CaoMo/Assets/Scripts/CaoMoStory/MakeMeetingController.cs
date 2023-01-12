604247d871da4bfa9adaec016370a229eb7cc2b4
LeoAtopos <275142760@qq.com>
2023-01-12 19:07:58 +0800

    先加一个选项

--- CaoMo/Assets/Scripts/CaoMoStory/MakeMeetingController.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/MakeMeetingController.cs
@@ -5,24 +5,31 @@ using DG.Tweening;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MakeMeetingController : MonoBehaviour
 {
-    public GameObject stage;
+    public GameObject stage;// 2944
     public GameObject huanPos;
     public GameObject huanTitleText;
+
     public GameObject huanLine;
     public GameObject guanPos;
     public GameObject guanTitleText;
     public GameObject guanLine;
+    public GameObject options;
     public GameObject subText;
-    
+
+    int ansCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        ansCount = 0;
         subText.SetActive(false);
         huanLine.SetActive(false);
         guanLine.SetActive(false);
+        options.SetActive(false);
         stage.transform.DOLocalMove(new Vector3(0, 0, 0), 8f).OnComplete(()=> StageInDone());
     }
 
@@ -33,7 +40,7 @@ public class MakeMeetingController : MonoBehaviour
     }
     void StageInDone()
     {
-        Invoke("HideTitles", 5);
+        Invoke("HideTitles", 2);
     }
     void HideTitles()
     {
@@ -60,13 +67,104 @@ public class MakeMeetingController : MonoBehaviour
     }
     void GuanLine1Done()
     {
+        options.SetActive(true);
+    }
+    internal void HuanAns(int t)
+    {
+        ansCount++;
+        options.SetActive(false);
         guanLine.SetActive(false);
-        Invoke("HuanLine2", 1.3f);
+        if(t == 1) huanLine.GetComponent<LineController>().SetText("好的，仲父");
+        if(t == 2) huanLine.GetComponent<LineController>().SetText("明白，亚父");
+        if (ansCount == 1) Invoke("HuanLine2", 0.1f);
+        if (ansCount == 2) Invoke("HuanLine3", 0.1f);
+        if (ansCount == 3) Invoke("HuanLine4", 0.1f);
     }
     void HuanLine2()
     {
-        huanLine.GetComponent<LineController>().SetText("好的，亚夫");
         huanLine.SetActive(true);
-        //Invoke("HuanLine1Done", 2.5f);
+        Invoke("HuanLine2Done", 2.5f);
+    }
+    void HuanLine2Done()
+    {
+        huanLine.SetActive(false);
+        Invoke("HuanLine2p", 1.3f);
+    }
+    void HuanLine2p()
+    {
+        huanLine.GetComponent<LineController>().SetText("先立大义，政治动员");
+        huanLine.SetActive(true);
+        Invoke("HuanLine2pDone", 2.5f);
+    }
+    void HuanLine2pDone()
+    {
+        huanLine.SetActive(false);
+        Invoke("GuanLine2", 1.3f);
+    }
+    void GuanLine2()
+    {
+        guanLine.GetComponent<LineController>().SetText("再会盟诸侯");
+        guanLine.SetActive(true);
+        Invoke("GuanLine2Done", 2.5f);
+    }
+    void GuanLine2Done()
+    {
+        options.SetActive(true);
+    }
+    void HuanLine3()
+    {
+        huanLine.SetActive(true);
+        Invoke("HuanLine3Done", 2.5f);
+    }
+    void HuanLine3Done()
+    {
+        huanLine.SetActive(false);
+        Invoke("HuanLine3p", 1.3f);
+    }
+    void HuanLine3p()
+    {
+        huanLine.GetComponent<LineController>().SetText("看谁不来，集体干他");
+        huanLine.SetActive(true);
+        Invoke("HuanLine3pDone", 2.5f);
+    }
+    void HuanLine3pDone()
+    {
+        huanLine.SetActive(false);
+        Invoke("GuanLine3", 1.3f);
+    }
+    void GuanLine3()
+    {
+        guanLine.GetComponent<LineController>().SetText("再会盟诸侯");
+        guanLine.SetActive(true);
+        Invoke("GuanLine3Done", 2.5f);
+    }
+    void GuanLine3Done()
+    {
+        options.SetActive(true);
+    }
+    void HuanLine4()
+    {
+        huanLine.SetActive(true);
+        Invoke("HuanLine4Done", 2.5f);
+    }
+    void HuanLine4Done()
+    {
+        huanLine.SetActive(false);
+        Invoke("HuanLine4p", 1.3f);
+    }
+    void HuanLine4p()
+    {
+        huanLine.GetComponent<LineController>().SetText("看谁不来，集体干他");
+        huanLine.SetActive(true);
+        Invoke("HuanLine4pDone", 2.5f);
+    }
+    void HuanLine4pDone()
+    {
+        huanLine.SetActive(false);
+        Invoke("GuanLine4", 1.3f);
+    }
+    void GuanLine4()
+    {
+
     }
 }
