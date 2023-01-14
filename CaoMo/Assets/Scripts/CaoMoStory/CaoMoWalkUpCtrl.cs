8e3eafd0b031bbe9d17464875b64949ec67927d7
LeoAtopos <275142760@qq.com>
2023-01-14 23:06:11 +0800

    舞台进入，旁白更新

--- CaoMo/Assets/Scripts/CaoMoStory/CaoMoWalkUpCtrl.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/CaoMoWalkUpCtrl.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
 
 public class CaoMoWalkUpCtrl : MonoBehaviour
 {
+    public GameObject stage;
+    public GameObject subText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        subText.SetActive(false);
+        stage.transform.DOLocalMove(new Vector3(0, 0, 0), 5f).OnComplete(() => StageInDone());
+        //stage.transform.DOLocalMove(new Vector3(0, 0, 0), 0.1f).OnComplete(() => StageInDone());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void StageInDone()
+    {
+        subText.SetActive(true);
     }
 }
