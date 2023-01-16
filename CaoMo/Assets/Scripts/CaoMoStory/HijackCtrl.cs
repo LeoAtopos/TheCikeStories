486cf0169a325f616d6afe4fed8c6fdac2d3390e
LeoAtopos <275142760@qq.com>
2023-01-16 11:36:21 +0800

    清理场景初始状态

--- CaoMo/Assets/Scripts/CaoMoStory/HijackCtrl.cs
+++ CaoMo/Assets/Scripts/CaoMoStory/HijackCtrl.cs
@@ -9,90 +9,95 @@ using System;
 
 public class HijackCtrl : MonoBehaviour
 {
-    //public GameObject stage;
-    //public GameObject subText;
+    public GameObject stage;
+    public GameObject subText;
 
     //public GameObject walkers;
-    //public Animator caoMoAnimation;
-    //public Animator luXiangAnimation;
-    //public Animator ZhuangAnimation;
-    //public GameObject opWalkers;
-    //public GameObject opWalkers2;
-    //public GameObject luXiang;
+    public Animator caoMoAnimation;
+    public Animator luXiangAnimation;
+    public Animator ZhuangAnimation;
+    public GameObject opWalkers;
+    public GameObject opWalkers2;
+    public GameObject luXiang;
     //public GameObject qiBingLine;
-    //public GameObject weiXiangLine;
-    //public GameObject xingXiangLine;
-    //public GameObject zhengXiangLine;
-    //public GameObject caoXiangLine;
-    //public GameObject chuXiangLine;
-    //public GameObject songXiangLine;
+    public GameObject weiXiangLine;
+    public GameObject xingXiangLine;
+    public GameObject zhengXiangLine;
+    public GameObject caoXiangLine;
+    public GameObject chuXiangLine;
+    public GameObject songXiangLine;
 
-    //bool isOKToMove;
+    bool isOKToMove;
     //bool isNoteHaveStopped;
 
     //public GameObject qiBingTrouble;
     //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    qiBingLine.SetActive(false);
-    //    weiXiangLine.SetActive(false);
-    //    xingXiangLine.SetActive(false);
-    //    zhengXiangLine.SetActive(false);
-    //    caoXiangLine.SetActive(false);
-    //    chuXiangLine.SetActive(false);
-    //    songXiangLine.SetActive(false);
-    //    subText.SetActive(false);
-    //    isOKToMove = false;
-    //    isNoteHaveStopped = true;
-    //    //stage.transform.DOLocalMove(new Vector3(0, 0, 0), 5f).OnComplete(() => StageInDone());
-    //    stage.transform.DOLocalMove(new Vector3(0, 0, 0), 0.1f).OnComplete(() => StageInDone());
-    //}
+    void Start()
+    {
+        //qiBingLine.SetActive(false);
+        weiXiangLine.SetActive(false);
+        xingXiangLine.SetActive(false);
+        zhengXiangLine.SetActive(false);
+        caoXiangLine.SetActive(false);
+        chuXiangLine.SetActive(false);
+        songXiangLine.SetActive(false);
+        subText.SetActive(false);
+        isOKToMove = false;
+        subText.SetActive(false);
+
+        //Vector3 sPos = stage.transform.localPosition;
+
+        //isNoteHaveStopped = true;
+        //stage.transform.DOLocalMove(new Vector3(0, 0, 0), 5f).OnComplete(() => StageInDone());
+        stage.transform.DOLocalMove(new Vector3(0, -445, 0), 3f).OnComplete(() => StageInDone());
+        //ZhuangLine1();
+    }
 
     //// -176,-707,-931,-1141,-1600
-    //void FixedUpdate()
-    //{
-    //    if (isOKToMove)
-    //    {
-    //        if (Input.GetMouseButton(0))
-    //        {
-    //            opWalkers.transform.localPosition -= 80f * opWalkers.transform.up * Time.fixedDeltaTime;
-    //            opWalkers2.transform.localPosition -= 80f * opWalkers.transform.up * Time.fixedDeltaTime;
-    //            caoMoAnimation.Play("CaoMoWalkUpMoving");
-    //            ZhuangAnimation.Play("ZhuangWalkUpMoving");
-    //            if (isNoteHaveStopped) luXiangAnimation.Play("LuXiangMoving");
-    //        }
-    //        if (isNoteHaveStopped && opWalkers.transform.localPosition.y < -176)
-    //            StopZhuang();
-    //        if (opWalkers.transform.localPosition.y < -315)
-    //            qiBingTrouble.transform.SetParent(opWalkers2.transform);
-    //        if (opWalkers.transform.localPosition.y < -607)
-    //            weiXiangLine.SetActive(true);
-    //        if (opWalkers.transform.localPosition.y < -657)
-    //        {
-    //            weiXiangLine.SetActive(false);
-    //            xingXiangLine.SetActive(true);
-    //        }
-    //        if (opWalkers.transform.localPosition.y < -831)
-    //            zhengXiangLine.SetActive(true);
-    //        if (opWalkers.transform.localPosition.y < -881)
-    //        {
-    //            zhengXiangLine.SetActive(false);
-    //            caoXiangLine.SetActive(true);
-    //        }
-    //        if (opWalkers.transform.localPosition.y < -1041)
-    //            songXiangLine.SetActive(true);
-    //        if (opWalkers.transform.localPosition.y < -1091)
-    //        {
-    //            songXiangLine.SetActive(false);
-    //            chuXiangLine.SetActive(true);
-    //        }
-    //        if (opWalkers.transform.localPosition.y < -1207)
-    //        {
-    //            SceneManager.LoadScene("111Hijack");
-    //        }
+    void FixedUpdate()
+    {
+        if (isOKToMove)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                opWalkers.transform.localPosition -= 80f * opWalkers.transform.up * Time.fixedDeltaTime;
+                opWalkers2.transform.localPosition -= 80f * opWalkers.transform.up * Time.fixedDeltaTime;
+                caoMoAnimation.Play("CaoMoWalkUpMoving");
+                ZhuangAnimation.Play("ZhuangWalkUpMoving");
+                //if (isNoteHaveStopped) luXiangAnimation.Play("LuXiangMoving");
+            }
+            //        if (isNoteHaveStopped && opWalkers.transform.localPosition.y < -176)
+            //            StopZhuang();
+            //        if (opWalkers.transform.localPosition.y < -315)
+            //            qiBingTrouble.transform.SetParent(opWalkers2.transform);
+            //        if (opWalkers.transform.localPosition.y < -607)
+            //            weiXiangLine.SetActive(true);
+            //        if (opWalkers.transform.localPosition.y < -657)
+            //        {
+            //            weiXiangLine.SetActive(false);
+            //            xingXiangLine.SetActive(true);
+            //        }
+            //        if (opWalkers.transform.localPosition.y < -831)
+            //            zhengXiangLine.SetActive(true);
+            //        if (opWalkers.transform.localPosition.y < -881)
+            //        {
+            //            zhengXiangLine.SetActive(false);
+            //            caoXiangLine.SetActive(true);
+            //        }
+            //        if (opWalkers.transform.localPosition.y < -1041)
+            //            songXiangLine.SetActive(true);
+            //        if (opWalkers.transform.localPosition.y < -1091)
+            //        {
+            //            songXiangLine.SetActive(false);
+            //            chuXiangLine.SetActive(true);
+            //        }
+            //        if (opWalkers.transform.localPosition.y < -1207)
+            //        {
+            //            SceneManager.LoadScene("111Hijack");
+            //        }
 
-    //    }
-    //}
+        }
+    }
 
     //private void StopZhuang()
     //{
@@ -111,9 +116,9 @@ public class HijackCtrl : MonoBehaviour
     //    isOKToMove = true;
     //    isNoteHaveStopped = false;
     //}
-    //void StageInDone()
-    //{
-    //    subText.SetActive(true);
-    //    isOKToMove = true;
-    //}
+    void StageInDone()
+    {
+        //subText.SetActive(false);
+        //isOKToMove = true;
+    }
 }
